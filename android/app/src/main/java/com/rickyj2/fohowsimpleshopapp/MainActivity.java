b6aac0c71d19c993e1e67b0953e1efb361c9d856
rickyj2 <rickyjunaidi02@gmail.com>
2026-04-29 14:23:26 +0700

    up

---
+++ android/app/src/main/java/com/rickyj2/fohowsimpleshopapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rickyj2.fohowsimpleshopapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rickyj2.fohowsimpleshopapp',
+  appName: 'simpleshopapp',
+  webDir: 'dist'
+};
+
+export default config;
